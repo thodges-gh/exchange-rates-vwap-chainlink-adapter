ef84b1b3a3dd5def8b7145f7fa4bf5c396ccf255
Thomas Hodges <me@thodges.com>
2019-01-11 14:17:39 -0600

    Add to repo

---
+++ README.md
@@ -0,0 +1,45 @@
+# Chainlink Kaiko External Adapter
+
+## Install
+
+```bash
+npm install
+```
+
+## Test
+
+```bash
+npm test
+```
+
+## Create the zip
+
+```bash
+zip -r cl-kaiko.zip .
+```
+
+## Install to AWS Lambda
+
+- In Lambda Functions, create function
+- On the Create function page:
+  - Give the function a name
+  - Use Node.js 8.10 for the runtime
+  - Choose an existing role or create a new one
+  - Click Create Function
+- Under Function code, select "Upload a .zip file" from the Code entry type drop-down
+- Click Upload and select the `cl-kaiko.zip` file
+- Handler should remain index.handler
+- Add the environment variable:
+  - Key: API_KEY
+  - Value: Your_API_key
+- Save
+
+## Install to GCP
+
+- In Functions, create a new function, choose to ZIP upload
+- Click Browse and select the `cl-kaiko.zip` file
+- Select a Storage Bucket to keep the zip in
+- Function to execute: gcpservice
+- Click More, Add variable
+  - NAME: API_KEY
+  - VALUE: Your_API_key
---
+++ test/index_test.js
@@ -0,0 +1,68 @@
+const assert = require('chai').assert;
+const createRequest = require('../index.js').createRequest;
+let moment = require("moment");
+
+describe('createRequest', () => {
+	context('with a given start time (-2 days)', () => {
+		const jobID = "278c97ffadb54a5bbb93cfec5f7b5503";
+		const req = {
+			id: jobID,
+			data: {
+				exchange: "stmp",
+				instrument: "btc-usd",
+				start_time: (moment.utc().format("X") - 172800) // Solidity: `now - 2 days`
+			}
+		};
+
+		it('returns data to the node', (done) => {
+			createRequest(req, (statusCode, data) => {
+				assert.equal(statusCode, 200);
+				assert.equal(data.jobRunID, jobID);
+				assert.isNotEmpty(data.data);
+				done();
+			});
+		});
+	});
+
+	context('with a given start time (-3 days) and end time (-2 days)', () => {
+		const jobID = "278c97ffadb54a5bbb93cfec5f7b5504";
+		const req = {
+			id: jobID,
+			data: {
+				exchange: "stmp",
+				instrument: "btc-usd",
+				start_time: (moment.utc().format("X") - 259200), // Solidity: `now - 3 days`
+				end_time: (moment.utc().format("X") - 172800) // Solidity: `now - 2 days`
+			}
+		};
+
+		it('returns data to the node', (done) => {
+			createRequest(req, (statusCode, data) => {
+				assert.equal(statusCode, 200);
+				assert.equal(data.jobRunID, jobID);
+				assert.isNotEmpty(data.data);
+				done();
+			});
+		});
+	});
+
+	context('without specify start or end times', () => {
+		const jobID = "278c97ffadb54a5bbb93cfec5f7b5505";
+		const req = {
+			id: jobID,
+			data: {
+				exchange: "stmp",
+				instrument: "btc-usd"
+			}
+		};
+
+		it('returns data to the node', (done) => {
+			createRequest(req, (statusCode, data) => {
+				assert.equal(statusCode, 200);
+				assert.equal(data.jobRunID, jobID);
+				assert.isNotEmpty(data.data);
+				done();
+			});
+		});
+	});
+});
